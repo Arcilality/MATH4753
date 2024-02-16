7d9fd4d6f12f9245b4d52e5cd6464bbc57fc9e5f
Scott Li <scott.l.li-1@ou.edu>
2024-02-15 21:54:59 -0600

    Initialize

---
+++ R/myquadplot.R
@@ -0,0 +1,13 @@
+#' Quadratic function
+#'
+#' @param x numeric vector
+#'
+#' @return Intercept of the quadratic function
+#' @export
+#'
+#' @examples
+#' myplot(x = 1:10)
+#'
+myplot=function(x){
+  0.86089580 +1.46959217*x  -0.02745726*x^2
+}
